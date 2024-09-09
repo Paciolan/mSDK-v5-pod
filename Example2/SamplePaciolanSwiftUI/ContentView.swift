14e24892ec2769f05c5614d2ffa7d03123d2dde7
gitlab-runner-mac-mini <pacmobileprov@paciolan.com>
2024-09-09 15:03:24 -0700

    Tag v5.105.240909

--- Example2/SamplePaciolanSwiftUI/ContentView.swift
+++ Example2/SamplePaciolanSwiftUI/ContentView.swift
@@ -4,7 +4,11 @@ import PaciolanSDK
 struct ContentView: View {
     
     @State var showSdk = false
+    @State var showManageTickets = false
+    @State var paciolanButtons = false
     @State var controller = PaciolanSDKViewController(string: "{\"channelCode\":\"msdk-sa\",\"sdkKey\":\"test2\",\"organizationId\":390,\"distributorCode\":\"CICD80\",\"applicationId\":\"com.paciolan.sdk\",\"uiOptions\":{\"accentColor\":\"#cc0000\",\"logoImage\":\"https://upload.wikimedia.org/wikipedia/en/thumb/2/28/Tulane_Green_Wave_logo.svg/1200px-Tulane_Green_Wave_logo.svg.png\"},\"route\":{\"name\":\"event-list\"},\"debug\":true,\"demo\":false}")!
+    
+    @State var controllerManageTickets = PaciolanSDKViewController(string: "{\"channelCode\":\"msdk-sa\",\"sdkKey\":\"test2\",\"organizationId\":390,\"distributorCode\":\"CICD80\",\"applicationId\":\"com.paciolan.sdk\",\"uiOptions\":{\"accentColor\":\"#cc0000\",\"logoImage\":\"https://upload.wikimedia.org/wikipedia/en/thumb/2/28/Tulane_Green_Wave_logo.svg/1200px-Tulane_Green_Wave_logo.svg.png\"},\"route\":{\"name\":\"ticket-management\"},\"debug\":true,\"demo\":false}")!
     
     var body: some View {
         VStack(spacing: 0) {
@@ -22,10 +26,34 @@ struct ContentView: View {
                     .onDisappear {
                         NotificationCenter.default.removeObserver(self, name: NSNotification.Name("ExitAppNotification"), object: nil)
                     }
-            } else {
-                Button("Open Paciolan SDK") {
-                    showSdk = true
+            } else if showManageTickets {
+                PaciolanViewRepresentable(controller: controllerManageTickets)
+                    .onAppear {
+                        NotificationCenter.default.addObserver(
+                            forName: NSNotification.Name("ExitAppNotification"),
+                            object: nil,
+                            queue: .main
+                        ) { notification in
+                            showManageTickets = false
+                        }
+                    }
+                    .onDisappear {
+                        NotificationCenter.default.removeObserver(self, name: NSNotification.Name("ExitAppNotification"), object: nil)
+                    }
+            } else if paciolanButtons {
+                VStack(spacing: 20) {
+                    Button("Manage Tickets") {
+                        showManageTickets = true
+                        showSdk = false
+                    }
+                    Button("Buy Tickets") {
+                        showManageTickets = false
+                        showSdk = true
+                    }
                 }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            } else {
+                Text("Home")
             }
             Spacer()
             Divider()
@@ -34,12 +62,8 @@ struct ContentView: View {
                     navigateAwayFromPaciolan()
                 }
                 Divider()
-                Button("Schedule") {
-                    navigateAwayFromPaciolan()
-                }
-                Divider()
-                Button("Open SDK") {
-                    showSdk = true
+                Button("Tickets") {
+                    paciolanButtons = true
                 }
             }
             .frame(maxWidth: .infinity)
@@ -57,6 +81,8 @@ struct ContentView: View {
                     shouldNavigate.boolValue
                 {
                     showSdk = false
+                    showManageTickets = false
+                    paciolanButtons = false
                 }
             }, rejecter: { code, message, error in
                 print("navAway Error: \(String(describing: code)) - \(String(describing: message))")
