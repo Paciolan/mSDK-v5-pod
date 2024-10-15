1a01b4231960ca6c4b28baf75234d6315e908674
jveronezi <jveronezi@fullstacklabs.co>
2024-10-15 16:29:26 -0300

    Tag v5.1.0

--- Example2/SamplePaciolanSwiftUI/ContentView.swift
+++ Example2/SamplePaciolanSwiftUI/ContentView.swift
@@ -2,19 +2,18 @@ import SwiftUI
 import PaciolanSDK
 
 struct ContentView: View {
+    
     @State var showSdk = false
     @State var showManageTickets = false
     @State var paciolanButtons = false
-    @State var controller: PaciolanSDKViewController? = nil
-
-    var sdkControllerString = "{\"channelCode\":\"msdk-sa\",\"sdkKey\":\"test2\",\"organizationId\":390,\"distributorCode\":\"CICD80\",\"applicationId\":\"com.paciolan.sdk\",\"uiOptions\":{\"accentColor\":\"#cc0000\",\"logoImage\":\"https://upload.wikimedia.org/wikipedia/en/thumb/2/28/Tulane_Green_Wave_logo.svg/1200px-Tulane_Green_Wave_logo.svg.png\"},\"route\":{\"name\":\"event-list\"},\"debug\":true,\"demo\":false}"
-
-    var manageTicketsControllerString = "{\"channelCode\":\"msdk-sa\",\"sdkKey\":\"test2\",\"organizationId\":390,\"distributorCode\":\"CICD80\",\"applicationId\":\"com.paciolan.sdk\",\"uiOptions\":{\"accentColor\":\"#cc0000\",\"logoImage\":\"https://upload.wikimedia.org/wikipedia/en/thumb/2/28/Tulane_Green_Wave_logo.svg/1200px-Tulane_Green_Wave_logo.svg.png\"},\"route\":{\"name\":\"ticket-management\"},\"debug\":true,\"demo\":false}"
-
+    @State var controller = PaciolanSDKViewController(string: "{\"channelCode\":\"msdk-sa\",\"sdkKey\":\"test2\",\"organizationId\":390,\"distributorCode\":\"CICD80\",\"applicationId\":\"com.paciolan.sdk\",\"uiOptions\":{\"accentColor\":\"#cc0000\",\"logoImage\":\"https://upload.wikimedia.org/wikipedia/en/thumb/2/28/Tulane_Green_Wave_logo.svg/1200px-Tulane_Green_Wave_logo.svg.png\"},\"route\":{\"name\":\"event-list\"},\"debug\":true,\"demo\":false}")!
+    
+    @State var controllerManageTickets = PaciolanSDKViewController(string: "{\"channelCode\":\"msdk-sa\",\"sdkKey\":\"test2\",\"organizationId\":390,\"distributorCode\":\"CICD80\",\"applicationId\":\"com.paciolan.sdk\",\"uiOptions\":{\"accentColor\":\"#cc0000\",\"logoImage\":\"https://upload.wikimedia.org/wikipedia/en/thumb/2/28/Tulane_Green_Wave_logo.svg/1200px-Tulane_Green_Wave_logo.svg.png\"},\"route\":{\"name\":\"ticket-management\"},\"debug\":true,\"demo\":false}")!
+    
     var body: some View {
         VStack(spacing: 0) {
             if showSdk {
-                PaciolanViewRepresentable(controller: controller!)
+                PaciolanViewRepresentable(controller: controller)
                     .onAppear {
                         NotificationCenter.default.addObserver(
                             forName: NSNotification.Name("ExitAppNotification"),
@@ -22,14 +21,13 @@ struct ContentView: View {
                             queue: .main
                         ) { notification in
                             showSdk = false
-                            controller = nil
                         }
                     }
                     .onDisappear {
                         NotificationCenter.default.removeObserver(self, name: NSNotification.Name("ExitAppNotification"), object: nil)
                     }
             } else if showManageTickets {
-                PaciolanViewRepresentable(controller: controller!)
+                PaciolanViewRepresentable(controller: controllerManageTickets)
                     .onAppear {
                         NotificationCenter.default.addObserver(
                             forName: NSNotification.Name("ExitAppNotification"),
@@ -37,7 +35,6 @@ struct ContentView: View {
                             queue: .main
                         ) { notification in
                             showManageTickets = false
-                            controller = nil
                         }
                     }
                     .onDisappear {
@@ -48,12 +45,10 @@ struct ContentView: View {
                     Button("Manage Tickets") {
                         showManageTickets = true
                         showSdk = false
-                        controller = PaciolanSDKViewController(string: manageTicketsControllerString)!
                     }
                     Button("Buy Tickets") {
                         showManageTickets = false
                         showSdk = true
-                        controller = PaciolanSDKViewController(string: sdkControllerString)!
                     }
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -76,17 +71,18 @@ struct ContentView: View {
             .frame(height: 30)
         }
     }
-
+    
     func navigateAwayFromPaciolan() {
         DispatchQueue.main.async {
-            controller?.navAway(fromPac: nil, resolver: { result in
-                print("navAway Success: \(result!)")
-
-                if let shouldNavigate = result as? NSNumber, shouldNavigate.boolValue {
+            controller.navAway(fromPac: nil, resolver: { result in
+                print("navAway Sucess: \(result!)")
+                    
+                if let shouldNavigate = result as? NSNumber,
+                    shouldNavigate.boolValue
+                {
                     showSdk = false
                     showManageTickets = false
                     paciolanButtons = false
-                    controller = nil
                 }
             }, rejecter: { code, message, error in
                 print("navAway Error: \(String(describing: code)) - \(String(describing: message))")
@@ -96,7 +92,9 @@ struct ContentView: View {
 }
 
 struct PaciolanViewRepresentable: UIViewControllerRepresentable {
+    
     var controller: PaciolanSDKViewController
+    
 
     func makeUIViewController(context: Context) -> PaciolanSDKViewController {
         controller.setTokenListener { token in
@@ -108,4 +106,5 @@ struct PaciolanViewRepresentable: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: PaciolanSDKViewController, context: Context) {
         print("updateUIViewController Test")
     }
+    
 }
