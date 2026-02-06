d6111d52b1f832be6dac367770bd43cb49d71e02
gitlab-runner-mac-mini <pacmobileprov@paciolan.com>
2026-02-06 06:23:56 -0800

    Tag v5.105.2602063882010

--- PaciolanSDK/Classes/PaciolanSDKViewController.m
+++ PaciolanSDK/Classes/PaciolanSDKViewController.m
@@ -33,7 +33,7 @@
 
 @implementation PaciolanSDKViewController
 @synthesize config;
-static NSString *INSTALLED_VERSION = @"5.105.2602053879143";
+static NSString *INSTALLED_VERSION = @"5.105.2602063882010";
 static TokenCallback tokenCallback;
 
 RCT_EXPORT_MODULE()
