6af741f8de2e1097937dcc59bba784cea1d91b00
gitlab-runner-mac-mini <pacmobileprov@paciolan.com>
2026-03-26 16:14:25 -0700

    Tag v5.5.0

--- PaciolanSDK/Classes/PaciolanSDKViewController.m
+++ PaciolanSDK/Classes/PaciolanSDKViewController.m
@@ -33,7 +33,7 @@
 
 @implementation PaciolanSDKViewController
 @synthesize config;
-static NSString *INSTALLED_VERSION = @"5.105.2603254084880";
+static NSString *INSTALLED_VERSION = @"5.5.0";
 static TokenCallback tokenCallback;
 
 RCT_EXPORT_MODULE()
@@ -65,7 +65,7 @@ RCT_EXPORT_MODULE()
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge {
-    [CodePush overrideAppVersion: @"5.105"];
+    [CodePush overrideAppVersion: @"5.5"];
 
     // set codepush custom server url
     NSString *serverUrl = @"https://pkg.paciolan.io/codepush/msdk/";
